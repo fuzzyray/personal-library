32b2002d6ca80e401e3def161a35c312fcd059f3
Paul Varner <fuzzyray@gmail.com>
2020-11-20 22:24:59 -0600

    Initial implementation of /api/books route

--- db/dbfunctions.js
+++ db/dbfunctions.js
@@ -21,8 +21,23 @@ const CommentSchema = new mongoose.Schema({
 const Comment = mongoose.model('Comment', CommentSchema);
 
 // Create
-const findOrCreateBook = (title, cb) => {
-  cb(null, {msg: 'Not Implemented'});
+const createOrFindBook = (title, cb) => {
+  const newBook = new Book({title: title});
+  Book.find({title: title}, (err, bookData) => {
+    if (err) {
+      cb(err, null);
+    } else if (Array.isArray(bookData) && bookData.length) {
+      cb(null, bookData[0]);
+    } else {
+      newBook.save((err, data) => {
+        if (err) {
+          cb(err, null);
+        } else {
+          cb(null, data);
+        }
+      });
+    }
+  });
 };
 
 const createComment = (comment, cb) => {
@@ -34,14 +49,44 @@ const getBookByTitle = (title, cb) => {
   cb(null, {msg: 'Not Implemented'});
 };
 
-const queryCommentsByBookId = (bookId, cb) => {
-  cb(null, {msg: 'Not Implemented'});
+const getCommentCountByBookId = (bookId, cb) => {
+  Comment.countDocuments({book: bookId}, (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else
+      cb(null, data);
+  });
+};
+
+const getCommentsByBookId = (bookId, cb) => {
+  Comment.find({book: bookId}, (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else
+      cb(null, data);
+  });
 };
 
 const getAllBooks = (cb) => {
+  Book.find()
+    .sort({title: 'ascending'})
+    .exec((err, data) => {
+      if (err) {
+        cb(err, null);
+      } else {
+        cb(null, data);
+      }
+    });
+};
+
+// Update
+const updateBookById = (bookId, cb) => {
   cb(null, {msg: 'Not Implemented'});
 };
-// Update
+
+const updateCommentById = (commentId, cb) => {
+  cb(null, {msg: 'Not Implemented'});
+};
 
 // Delete
 const deleteBookById = (bookId, cb) => {
@@ -49,7 +94,26 @@ const deleteBookById = (bookId, cb) => {
 };
 
 const deleteAllBooks = (cb) => {
-  cb(null, {msg: 'Not Implemented'});
+  Comment.deleteMany({}, (err, data) => {
+    if (err) {
+      cb(err, null)
+    } else {
+      console.log('comments', data)
+      Book.deleteMany({}, (err, data) => {
+        if (err) {
+          cb(err, null)
+        } else {
+          console.log('books', data)
+          cb(null, data)
+        }
+      })
+    }
+  })
 };
 
 exports.connect = connect;
+exports.createOrFindBook = createOrFindBook;
+exports.getAllBooks = getAllBooks;
+exports.getCommentsByBookId = getCommentsByBookId;
+exports.getCommentCountByBookId = getCommentCountByBookId;
+exports.deleteAllBooks = deleteAllBooks;
--- routes/api.js
+++ routes/api.js
@@ -8,21 +8,91 @@
 
 'use strict';
 
+const createOrFindBook = require('../db/dbfunctions').createOrFindBook;
+const getAllBooks = require('../db/dbfunctions').getAllBooks;
+const getCommentCountByBookId = require(
+  '../db/dbfunctions').getCommentCountByBookId;
+const deleteAllBooks = require('../db/dbfunctions').deleteAllBooks;
+
+const getBookComments = (bookId) => {
+  return new Promise((resolve, reject) => {
+    getCommentCountByBookId(bookId, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const createBookResult = async (data) => {
+  const result = [];
+  for (let i = 0; i < data.length; i++) {
+    const commentCount = await getBookComments(data[i]._id);
+    result.push({
+      '_id': data[i]._id,
+      'title': data[i].title,
+      'commentcount': commentCount,
+    });
+  }
+  return result;
+};
+
 module.exports = function(app) {
 
   app.route('/api/books')
+    //response will be array of book objects
+    //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     .get(function(req, res) {
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      getAllBooks((err, data) => {
+        if (err) {
+          console.error(err);
+          res.status(400).send(err._message);
+        } else {
+          const result = createBookResult(data)
+            .then(result => {
+              res.json(result);
+            })
+            .catch(err => {
+              console.error(err);
+              res.status(400).send(err._message);
+            });
+        }
+      });
     })
 
     .post(function(req, res) {
-      let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      const title = req.body.title;
+      if (!!title) {
+        createOrFindBook(title, (err, data) => {
+          if (err) {
+            console.error(err);
+            res.status(400).send(err._message);
+          } else {
+            res.json(
+              {
+                _id: data._id,
+                title: data.title,
+              });
+          }
+        });
+      } else {
+        res.send('missing required field title');
+      }
     })
 
     .delete(function(req, res) {
       //if successful response will be 'complete delete successful'
+      deleteAllBooks((err, data) => {
+        if (err) {
+          console.error(err);
+          res.status(400).send(err._message);
+        } else {
+          res.send('complete delete successful');
+        }
+      });
     });
 
   app.route('/api/books/:id')
